13dd6c65520aeb18316c6c74c6fbf3f7387afe87
Thiha_tz <veb01-wi240011@sankogakuen.jp>
2025-10-01 02:00:32 +0900

    Add files via upload

--- js/songRecommendation.js
+++ js/songRecommendation.js
@@ -18,19 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
     Melancholic: [
       "https://open.spotify.com/embed/track/5UXJzLFdBn6u9FJTCnoHrH?utm_source=generator",
       "https://open.spotify.com/embed/track/4q3biaQQoADspV16lbsfPH?utm_source=generator",
-      "https://open.spotify.com/embed/track/5gguiSurLMRJwKPTGbEj5r?utm_source=generator",
-      "https://open.spotify.com/embed/track/74h6p7El7eyV6HKvTefpIJ?utm_source=generator",
-      "https://open.spotify.com/embed/track/4SjXHwsZevaRo1M2EQgHf6?utm_source=generator",
-      "https://open.spotify.com/embed/track/3pTHZHX0MRE7xYeiTEWlaM?utm_source=generator",
-      "https://open.spotify.com/embed/track/7wfxf9QXBIWgBx1aG5QWhR?utm_source=generator",
-      "https://open.spotify.com/embed/track/6DRZmJa38MaMNthwG3fCBD?utm_source=generator",
-      "https://open.spotify.com/embed/track/16bo9rjhuBfM6mUFKpSdRJ?utm_source=generator"
+      "https://open.spotify.com/embed/track/2X71ww8wImSYbw4s0Mr2ur?utm_source=generator",
+      "https://open.spotify.com/embed/track/66h3xcrTooMC9AxV8g0Y75?utm_source=generator",
+      "https://open.spotify.com/embed/track/2mlNgAeIBnL78ZriXgrRHz?utm_source=generator",
+      "https://open.spotify.com/embed/track/1B2dsxhiNChPRUPlxyhHck?utm_source=generator",
+      "https://open.spotify.com/embed/track/38CFljBjwKy4CS4mx6u9es?utm_source=generator",
+      "https://open.spotify.com/embed/track/046Gh1kXtPynYPaKep5M8E?utm_source=generator",
+      "https://open.spotify.com/embed/track/1XqZ4xJ49New05avVfzwiY?utm_source=generator"
     ],
     
     Energetic: [
-       "https://open.spotify.com/embed/track/1mXVgsBdtIVeCLJnSnmtdV?utm_source=generator",
-      "https://open.spotify.com/embed/track/0DGh73t9eaZA3X3ZXHf4Qe?utm_source=generator",
-      "https://open.spotify.com/embed/track/5gguiSurLMRJwKPTGbEj5r?utm_source=generator",
+      "https://open.spotify.com/embed/track/1XqZ4xJ49New05avVfzwiY?utm_source=generator",
+      "https://open.spotify.com/embed/track/71rw9MB77BR8LHMFZK8gKV?utm_source=generator",
+      "https://open.spotify.com/embed/track/1SVOLTmC627fn0XY9ltvx2?utm_source=generator",
       "https://open.spotify.com/embed/track/74h6p7El7eyV6HKvTefpIJ?utm_source=generator",
       "https://open.spotify.com/embed/track/4SjXHwsZevaRo1M2EQgHf6?utm_source=generator",
       "https://open.spotify.com/embed/track/3pTHZHX0MRE7xYeiTEWlaM?utm_source=generator",
